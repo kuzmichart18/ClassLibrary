5ffa91b0b3990219282cbe6736c0a5cde736d997
student <student@PC-VOL-231-21>
2023-03-16 12:54:02 +0300

    Добавьте файлы проекта.

---
+++ ClassLibraryTests/PasswordCheckerTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.Tests
+{
+    [TestClass()]
+    public class PasswordCheckerTests
+    {
+        [TestMethod()]
+        public void Check_8Symbols_ReturnsTrue()
+        {
+            // Arrange.
+            string password = "ASqw12$$";
+            bool expected = true;
+
+            // Act.
+            bool actual = PasswordChecker.ValidatePasswodr(password);
+
+            // Assert.
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Check_4Symbols_ReturnsFalse()
+        {
+            // Arrange.
+            string password = "Aq1$";
+            bool expected = false;
+
+            // Act.
+            bool actual = PasswordChecker.ValidatePasswodr(password);
+
+            // Assert.
+            Assert.IsFalse(actual);
+        }
+    }
+}
